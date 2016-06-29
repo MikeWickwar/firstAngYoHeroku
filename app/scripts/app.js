928b951c76f49936a247ae825d86df07f21e404a
Mike Wickwar <mike.wickwar@gmail.com>
2016-06-29 15:10:19 -0600

    create angular app using yeoman deploying to heroku

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lasvApp
+ * @description
+ * # lasvApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lasvApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lasvApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lasvApp
+ */
+angular.module('lasvApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
